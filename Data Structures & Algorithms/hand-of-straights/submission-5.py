c6ffc62aa7062236e1644ec664a314bc8d0ba8d5
Mankirat Singh <53979716+Mankirt@users.noreply.github.com>
2026-09-02 12:11:29 -0700

    Add: hand-of-straights - submission-5

---
+++ Data Structures & Algorithms/hand-of-straights/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+        counter = Counter(hand)
+        heap = []
+        for key in counter:
+            heapq.heappush(heap, key)
+
+        while heap:
+            crr = heap[0]
+            for i in range(crr,crr+groupSize):
+                if i not in counter:
+                    return False
+                counter[i] -= 1
+                if counter[i] == 0:
+                    if i != heap[0]:
+                        return False
+                    heapq.heappop(heap)
+        return True
